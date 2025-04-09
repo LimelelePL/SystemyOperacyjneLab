1204103bc605229d6370a44708ec50f3ddb60ce8
Dominik <DudekDominik80@gmail.com>
2025-04-09 12:03:45 +0200

    jjjjj

--- Zadanie2So/src/FCFS.java
+++ Zadanie2So/src/FCFS.java
@@ -26,6 +26,7 @@ public class FCFS extends Algoritm implements Scheduler {
 
             process.setCompleted(true);
             addDoneProcess();
+
             addWaitTime(currentTime - process.getArrivalTime());
 
         }
